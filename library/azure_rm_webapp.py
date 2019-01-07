3e94438d9ba068b18089db18cf1357f2af12cfd9
Yunge Zhu <yungez@microsoft.com>
2019-01-07 16:30:09 +0800

    test aa

--- library/azure_rm_webapp.py
+++ library/azure_rm_webapp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2018 Yunge Zhu, <yungez@microsoft.com>aaa
+# Copyright (c) 2018 Yunge Zhu, <yungez@microsoft.com>aa
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
